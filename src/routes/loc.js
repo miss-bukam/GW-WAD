b0fa64c81dbfdfd133d7f5fb5b78d1f0ec6dcb07
miss-bukam <115629298+miss-bukam@users.noreply.github.com>
2024-01-09 11:28:29 +0100

    Update loc.js
    alles wurde in der Master branch gemacht nun wurde in die main gepushed

--- src/routes/loc.js
+++ src/routes/loc.js
@@ -1,66 +1,119 @@
 let express = require('express');
 let router = express.Router();
 
+const { ObjectId } = require('mongodb');
+
 const mongoCRUDs = require('../db/mongoCRUDs');
 
-//für den BasisPfad /loc
-app.use('/loc', mongoCRUDs);
-
-app.listen(8000, () => {
-  console.log('Server is running on port 8000');
-});
-
-// Wird bei POST http://localhost:8000/users aufgerufen 
-
+const collectionName = "loc";
 
 // Wird bei GET http://localhost:8000/users aufgerufen 
-router.get('/get', async function(req, res) {
-  try {
-    //let userDoc = await mongo_cruds.findOneUser("admina", "pass1234");
-    let locs = await mongoCRUDs.findAllLocs();
-    if(locs)
-      res.status(200).json(locs);
-    else {
-      res.status(404).send(`Locations not found!`);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("Something is not right!!");
-  }
-});
-
-// Wird bei /loc/id http://localhost:8000/loc/1 aufgerufen 
-router.get('/:id', async function(req, res) {
-  try {
-    // Konvertiere die ID in eine Zahl (int32)
-    const locID = parseInt(req.params.id);
-    const loc = await mongoCRUDs.findOneLocation(locID);
-
-    if (loc) {
-      res.status(200).json(loc);
-    } else {
-      res.status(404).send(`Location not found!`);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-
-
-// Wird bei POST http://localhost:8000/users aufgerufen 
-router.post('/', function(req, res) {
+router.get('/', async function(req, res) {
+    const userId = req.userId 
   
-  let locationBody = req.body;  
-  console.log (locationBody);
+    try {
+      //let userDoc = await mongo_cruds.findOneUser("admina", "pass1234");
+      let locs = await mongoCRUDs.findAllLocs();
+      if(locs)
+        res.status(200).json(locs);
+      else {
+        res.status(404).send(`Locations not found!`);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(400).send("Something is not right!!");
+    }
+  });
+
+// Wird bei GET <id> http://localhost:8000/users aufgerufen 
+router.get("/:id", async (req, res) => { 
+    console.log("getPost: called");
+    try {
+        const collection = mongoCRUDs.collection(collectionName); // Hier die Collection initialisieren
+        const query = {_id: new ObjectId(req.params.id)};
+        const result = await collection.findOneLocation(query);
+
+        console.log(result);
+        if(result)
+            res.status(200).json(result);
+        else {
+            res.status(404).send(`Location not found!`);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Something is not right!!");
+    }
 });
 
-// Wird bei PUT  http://localhost:8000/users aufgerufen 
 
+// Wird bei POST http://localhost:8000/post aufgerufen 
+router.post("/", async (req, res) => {
+    console.log("addPost: called")
+    
+    try {
+        const collection = mongoCRUDs.collection(collectionName)
+        
+        const location = {
+            name: req.body.name,
+            description: req.body.description,
+            street: req.body.street,
+            zip: req.body.zip,
+            city: req.body.city,
+            state: req.body.state,
+            lat: req.body.lat,
+            lng: req.body.lng
+        }
 
-// Wird bei DELETE http://localhost:8000/users aufgerufen 
+        await collection.insertOne(location)
 
+        res.status(201).end()
+    } catch (err) {
+        console.log(err)
+    }
+});
 
+// Wird bei POST http://localhost:8000/ aufgerufen 
+router.delete("/:id", async (req, res) => {
+    console.log("deletePost: called")
+
+    try {
+        const collection = mongoCRUDs.collection(collectionName)
+
+        const query = {_id: new ObjectId(req.params.id)}
+
+        await collection.deleteOneLocation(query)
+        res.status(204).end()
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    console.log("updatePost: called")
+
+    try {
+        const collection = mongoCRUDs.collection(collectionName)
+        
+        const filter = {_id: new ObjectId(req.params.id)}
+
+        const update = {
+            $set: {
+                name: req.body.name,
+                description: req.body.description,
+                street: req.body.street,
+                zip: req.body.zip,
+                city: req.body.city,
+                state: req.body.state,
+                lat: req.body.lat,
+                lng: req.body.lng
+            }
+        }
+
+        await collection.updateOne(filter, update)
+        res.status(204).end()
+    } catch (err) {
+        console.log(err)
+    }
+});
 
 module.exports = router;
